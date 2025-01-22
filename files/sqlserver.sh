f01805cdd16a4685707f1581f5a1cf7864ba0fcd
ErikAhlinSWE <erik.ahlin@live.se>
2025-01-22 16:12:03 +0100

    added --skip-ssl

--- files/sqlserver.sh
+++ files/sqlserver.sh
@@ -6,7 +6,7 @@
 
 FILENAME=$(date "+%Y-%m-%d-%H-%M-%S")
 #sqlcmd -S ${sql_host} -U ${sql_user} -P ${sql_password} -Q "BACKUP DATABASE ${sql_database} TO DISK='/var/opt/mssql/data/${sql_database}$FILENAME'"
-mysqldump -h ${sql_host}   -u${sql_user} -p${sql_password} ${sql_database} --flush-logs --master-data=2 --delete-master-logs | gzip >  /opt/src/${sql_database}$FILENAME.sql.gz
+mysqldump -h ${sql_host}   -u${sql_user} -p${sql_password} ${sql_database} --flush-logs --master-data=2 --delete-master-logs --skip-ssl | gzip >  /opt/src/${sql_database}$FILENAME.sql.gz
 
 # main entry point to run s3cmd
 #
